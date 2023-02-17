a94c7de23a0a2018bb9e20df197e9d89a1429c35
Rogibb111 <Rogibb111@users.noreply.github.com>
2023-02-17 10:00:54 -0700

    Adding generic axis type and Axis Data type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,7 @@
-export type AxisLengths = { 
-    x: number;
-    y: number;
+export type Axis<T> = { 
+    x: T;
+    y: T;
 }
+
+export type AxisData = { begin: number, end: number, interval: number, type: 'date' | 'number' }
+    | Array<String>;
