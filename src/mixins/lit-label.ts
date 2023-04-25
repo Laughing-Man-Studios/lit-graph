cfd87afcbecc05b041611c0f29caa18004de5dc8
Robert Gibb <Rogibb111@users.noreply.github.com>
2023-04-24 19:49:10 -0600

    feat(display): Adding Lit-Label Mixin
    [Issue 16] Adding Lit-Label Mixin (#29)
    - Adding new mixin to render the X and Y axis labels
    - Adding tests to see if labels exist or not

---
+++ src/mixins/lit-label.ts
@@ -0,0 +1,34 @@
+import { css,  LitElement, svg } from 'lit';
+import { Axis } from '../types';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Constructor<T = {}> = new (...args: any[]) => T;
+
+export declare class LitLabelInterface {
+    renderLabels(axisLabels: Axis<string, string>): unknown;
+}
+
+export const LitLabelMixin = <T extends Constructor<LitElement>>(superClass: T) => {
+    class LitLabelClass extends superClass {
+
+        static styles = (css` 
+            #labels text.y {
+                transform-box: fill-box;
+	            transform: rotate(-90deg);
+	            transform-origin: center;
+            }
+        `);
+
+        renderLabels(axisLabels: Axis<string, string>) {
+            const { x, y } = axisLabels;
+
+            return (svg`
+                <g id="labels">
+                    <text class="x" x="50%" y="100%">${x}</text>
+                    <text class="y" y="50%">${y}</text>
+                <g>
+            `);
+        }
+    }
+    return LitLabelClass as Constructor<LitLabelInterface> & T;
+};
