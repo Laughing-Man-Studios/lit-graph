b0d113f6840fd7468fe0a3c26a340e08f1db27da
Rogibb111 <Rogibb111@users.noreply.github.com>
2023-05-05 08:13:14 -0600

    Adding test for dates

--- src/test/lit-graph_test.ts
+++ src/test/lit-graph_test.ts
@@ -2,15 +2,42 @@ import LitGraph from '../lit-graph';
 
 import {fixture, assert} from '@open-wc/testing';
 import {html} from 'lit/static-html.js';
+import { TemplateResult } from 'lit';
 
 const ERRORS = {
     NO_DATA: 'No data was passed to Lit-Graph',
 };
 
-const data = [
-    {x: 1, y: 1},
-    {x: 2, y: 2},
-];
+const TEST_DATA = {
+    NUMBERS: [
+        {x: 1, y: 1},
+        {x : 2, y: 2},
+    ],
+    DATES: [
+        { x: '2023-05-05T12:02:27.034Z', y: '2023-05-05T12:02:27.034Z' },
+        { x: '2023-06-05T12:02:27.034Z', y: '2023-06-05T12:02:27.034Z' },
+        { x: '2023-07-05T12:02:27.034Z', y: '2023-07-05T12:02:27.034Z' }
+    ]
+};
+
+async function assertRender(renderHTML: TemplateResult) {
+    const el = (await fixture(renderHTML)) as LitGraph;
+    const labels = el.renderRoot.querySelector('#labels');
+    const xGridLines = el.renderRoot.querySelector('#xGridLines');
+    const yGridLines = el.renderRoot.querySelector('#yGridLines');
+    const xLabels = el.renderRoot.querySelector('#xLabels');
+    const yLabels = el.renderRoot.querySelector('#yLabels');
+    const plotCircles = el.renderRoot.querySelector('#PlotCircles');
+    const plotLines = el.renderRoot.querySelector('#PlotLines');
+
+    assert.exists(labels, 'Couldnt find labels grouping');
+    assert.exists(xGridLines, 'Couldnt find X Grid Lines grouping');
+    assert.exists(yGridLines, 'Couldnt find Y Grid Lines grouping');
+    assert.exists(xLabels, 'Couldnt find X Labels grouping');
+    assert.exists(yLabels, 'Couldnt find Y Labels grouping');
+    assert.exists(plotCircles, 'Couldnt find Plot Circles grouping');
+    assert.exists(plotLines, 'Couldnt find Plot Lines grouping');
+}
 
 suite('lit-graph', () => {
     test('is defined', () => {
@@ -42,29 +69,27 @@ suite('lit-graph', () => {
         return assert.fail('Did not throw an error');
     });
 
-    test('calls all mixin render functions', async () => {
+    test('calls all mixin render functions with numbers as data', async () => {
         const renderHTML = html`
             <lit-graph
                 x-label="Test"
                 y-label="Test"
-                .data="${data}"
+                .data="${TEST_DATA.NUMBERS}"
             ></lit-graph>
         `;
-        const el = (await fixture(renderHTML)) as LitGraph;
-        const labels = el.renderRoot.querySelector('#labels');
-        const xGridLines = el.renderRoot.querySelector('#xGridLines');
-        const yGridLines = el.renderRoot.querySelector('#yGridLines');
-        const xLabels = el.renderRoot.querySelector('#xLabels');
-        const yLabels = el.renderRoot.querySelector('#yLabels');
-        const plotCircles = el.renderRoot.querySelector('#PlotCircles');
-        const plotLines = el.renderRoot.querySelector('#PlotLines');
 
-        assert.exists(labels, 'Couldnt find labels grouping');
-        assert.exists(xGridLines, 'Couldnt find X Grid Lines grouping');
-        assert.exists(yGridLines, 'Couldnt find Y Grid Lines grouping');
-        assert.exists(xLabels, 'Couldnt find X Labels grouping');
-        assert.exists(yLabels, 'Couldnt find Y Labels grouping');
-        assert.exists(plotCircles, 'Couldnt find Plot Circles grouping');
-        assert.exists(plotLines, 'Couldnt find Plot Lines grouping');
+        assertRender(renderHTML);
+    });
+
+    test('calls all mixin render functions with dates as data', async () => {
+        const renderHTML = html`
+            <lit-graph
+                x-label="Test"
+                y-label="Test"
+                .data="${TEST_DATA.DATES}"
+            ></lit-graph>
+        `;
+
+        assertRender(renderHTML);
     });
 });
